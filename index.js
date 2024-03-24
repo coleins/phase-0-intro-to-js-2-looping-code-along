b3d267a77db6512d2354866526b254a06568b2c0
Collins Ngetich <collinsngetichk@gmail.com>
2024-03-24 13:51:00 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+function writeCards(names, giftType) {
+  return names.map(
+    (name) => `Thank you, ${name}, for the wonderful ${giftType} gift!`
+  );
+}
+function countDown(startingNumber) {
+  for (let i = startingNumber; i >= 0; i--) {
+    console.log(i);
+  }
+}
